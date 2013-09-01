d8be39f09112e210056eec24af7780a642046149
Catalin Ilinca <c@talin.ro>
2013-09-02 01:09:43 +0300

    Inital import.

---
+++ README.md
@@ -0,0 +1,4 @@
+HR Map
+=====
+
+HR tool for managing departments and employees. It also generate an organigram for the structure.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hrmap::Application.config.secret_key_base = '31444f7c9e2c47082c1b17ef09fab8da5fbc3101a6a359f816bc8c43423aff7962b94ff91c9c2bdbbd45a53d2f713c2f1676fe740969eddd07ead171fc874d16'
